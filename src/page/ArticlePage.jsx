d3d03d3ff5ee9dd6cd6ae62753bb46eb80e092ae
Vishnu <thenormvg@gmail.com>
2024-07-01 22:02:57 +0530

    bhai sun contacts page ban gaya h

--- src/page/ArticlePage.jsx
+++ src/page/ArticlePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Head from '../components/Head'
+
+import "../css/ArticlePageStyle.css"
 
 function ArticlePage() {
   return (
-    <div>
+    <div id='article-box'>
       
     </div>
   )
