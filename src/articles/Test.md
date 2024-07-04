f0dfbc18b97a22b8d2129e4f2f40bd62628edf9a
Vishnu <thenormvg@gmail.com>
2024-07-04 14:08:20 +0530

    bhaiiiiii ! markdown wala system ho gaya

--- src/articles/Test.md
+++ src/articles/Test.md
@@ -1,3 +1,5 @@
+# Haaa bhai kesa ha
+
 It looks like you're using the `react-fast-marquee` library to create a scrolling effect on your `ContactPage`. However, if the `Marquee` component with the `direction="down"` is causing issues where the element is randomly being removed or not displaying correctly, you might need to look into alternative ways to achieve the desired effect or troubleshoot the `react-fast-marquee` implementation.
 
 ### Troubleshooting `react-fast-marquee`
@@ -15,6 +17,8 @@ If `react-fast-marquee` continues to cause problems, you can achieve a similar s
 #### CSS
 
 Add the following CSS to your `ContactPageStyle.css`:
+
+
 
 ```css
 #contact-box {
@@ -61,25 +65,25 @@ Add the following CSS to your `ContactPageStyle.css`:
 Update your `ContactPage` component:
 
 ```jsx
-import React from 'react';
+import React from "react";
 import FlowBar from "../assets/flow-bar-full.png";
 import "../css/ContactPageStyle.css";
 import subBarimg from "../assets/subBarCon.png";
 
 function ContactPage() {
   return (
-    <div id='contact-box'>
-      <div id='journal-box-head'>
-        <div id='journal-box-title'>Nexus</div>
-        <div id='journal-box-sub'>
-          <img id='journal-box-sub-img' src={subBarimg} alt="" />
-          <div id='journal-box-sub-head'>
+    <div id="contact-box">
+      <div id="journal-box-head">
+        <div id="journal-box-title">Nexus</div>
+        <div id="journal-box-sub">
+          <img id="journal-box-sub-img" src={subBarimg} alt="" />
+          <div id="journal-box-sub-head">
             Please no gang signs. No, throw it up. I'm kidding.
           </div>
         </div>
       </div>
 
-      <div id='cb-mid-bar'>
+      <div id="cb-mid-bar">
         <img src={FlowBar} alt="" />
       </div>
     </div>
@@ -92,7 +96,6 @@ export default ContactPage;
 ### Explanation
 
 - **CSS Animation**: The `@keyframes scrollDown` animation moves the `#cb-mid-bar` element from top to bottom continuously. You can adjust the `10s` duration to make the animation slower or faster.
-  
 - **Positioning**: The `#contact-box` has `overflow: hidden` to ensure that only the part of the `#cb-mid-bar` within its boundaries is visible, creating a scrolling effect.
 
 This approach avoids potential issues with third-party libraries and gives you more control over the scrolling effect.
--- src/page/ArticlePage.jsx
+++ src/page/ArticlePage.jsx
@@ -1,42 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-import "../css/ArticlePageStyle.css"
+import "../css/ArticlePageStyle.css";
 
-import BannerImgDef from "../assets/def-article.png"
-import DownWave from "../assets/down-wave.png"
+import BannerImgDef from "../assets/def-article.png";
+import DownWave from "../assets/down-wave.png";
 
-import MyMdx from "../articles/Test.mdx"
-import { MDXProvider } from '@mdx-js/react';
-
-
-
+import MyMd from "../articles/Test.md";
+// import { MDXProvider } from '@mdx-js/react';
+import Markdown from "react-markdown";
 
 function ArticlePage() {
+  const [markdown, setMarkdown] = useState("");
+  
+  useEffect(() => {
+    fetch(MyMd)
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
+
+  console.log(markdown)
+
   return (
-    <div id='article-box'>
-        <div id='article-box-shell'>
-            <div id='article-box-banner'  style={{backgroundImage:`url( ${BannerImgDef} )`}}>
-
-              <div id='article-ta'>
-                <div id='article-t' >Yep this is a title , and i’m really sorry for all this time .</div>
-                <div id='article-a' >by Vishnu Gupta</div>
-              </div>
+    <div id="article-box">
+      <div id="article-box-shell">
+        <div
+          id="article-box-banner"
+          style={{ backgroundImage: `url( ${BannerImgDef} )` }}
+        >
+          <div id="article-ta">
+            <div id="article-t">
+              Yep this is a title , and i’m really sorry for all this time .
             </div>
-
-            
-              <div id='article-datetime' >10.12.2023</div>
-            
-            <div id='article-main-content'>
-            <MDXProvider>
-            < MyMdx />
-            </MDXProvider>
-            </div>
-
-            <div id='article-bottom-img'><img src={DownWave} alt="" /></div>
-
+            <div id="article-a">by Vishnu Gupta</div>
+          </div>
         </div>
+
+        <div id="article-datetime">10.12.2023</div>
+
+        <div id="article-main-content">
+          <Markdown>
+
+          {markdown}
+
+          </Markdown>
+        </div>
+
+        <div id="article-bottom-img">
+          <img src={DownWave} alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ArticlePage
+export default ArticlePage;
